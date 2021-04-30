8f63fa007b08cb1f059a90891e4d7bbbf57bb6f3
AndyRama <andyramaroson@gmail.com>
2021-05-01 00:29:13 +0200

    bakckup

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+
+# Location Property
+
+Location Property platform build using Ruby on Rails 6. 
+
+Adresse :"https://location-property-andy.herokuapp.com"
+
+# [🔗 View Deployed Version](https://location-property-andy.herokuapp.com)
+
+user: ando@gmail.com   pwd: 42KgrDYiewMgySx            user: recruteur@gmail.com   pwd: Pbxk1064
+
+## Main features:
+
+1. The sign up / sign in / edit account using the devise gem.
+
+2. Add properties (with a photo that gets uploaded with Cloudinary, active_storage_validations,image_processing gem) and build a property profile page.
+
+3. Show property agent details and related properties from the same agent.
+
+4. Updating the property profile page to add functionality to view the agent's phone number, visit their profile and send them an email. I also added the ability to choose if a property is a rental or for sale and set the move in date for users to see on the profile.
+
+5. Updating the agent profile page to show a cover image, user details and stats about properties leased and sold. Users can now change the status of properties to mark them as leased or sold from their user account when signed in. I also added blog posts to the app and improve the homepage styling. In the backend I added an accounts page for admin accounts and ability to add/edit/update blog entries.
+
+I also added some responsive css to make our platform mobile / tablet / iPad friendly.
+
+## The stack
+      
+  Ubuntu 18.04
+  Ruby 2.6.6
+  Rails
+  Javascript
+  HTML/CSS
+  ProgreSQL
+  Git/Github
+  Dot ENV
+  Node SASS
+  CSS Animations
+  Flexbox
+
+## The Screenshots
+
+![alt text](https://github.com/AndyRama/location_property/blob/master/app/assets/images/HomeLocation.png?raw=true)
+
+![alt text](https://github.com/AndyRama/location_property/blob/master/app/assets/images/Propertylist.png?raw=true)
+
+![alt text](https://github.com/AndyRama/location_property/blob/master/app/assets/images/Propertypage.png?raw=true)
+
+![alt text](https://github.com/AndyRama/location_property/blob/master/app/assets/images/AgencyList.png?raw=true)
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,72 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding DB:"
+print "\n Création reussie: '.'"
+print " - "
+print " Déjà existant: '-'"
+print " - "
+print " Création echouée: 'X'"
+
+Users.destroy_all
+
+puts "\n\n Création de 1 Utilisateurs"
+
+user1 = User.new(first_name:"ando",last_name:"alain",email:"andy@gmail.com",password: "Pbxk1984",url: nil, admin: true, image: nil, company:"A2RSII",number:"0679554022" ,details: "",cover_image: nil)
+
+puts "\n\n Création d'une image pour profil Utilisateurs"
+
+if user1.save
+  user1.image.attach(io: open("https://res.cloudinary.com/dbtnehfrf/image/upload/v1619189911/vkf87wxq2a9imtiaixy7jkkf3kpw.png"), filename:'user1')
+  print('.')
+else
+  user1 = User.where(email: "andy@gmail.com").first
+  if user1
+    print('-')
+  else
+    print('X')
+  end
+end
+
+puts "\n\n Création d'une image cover pour profil Utilisateurs"
+
+if user1.save
+  user1.cover_image.attach(io: open("https://res.cloudinary.com/dbtnehfrf/image/upload/v1619106936/og1hrs3zmzr65qre38sjnu205p8n.jpg"), filename:'user1')
+  print('.')
+else
+  user1 = User.where(email: "andy@gmail.com").first
+  if user1
+    print('-')
+  else
+    print('X')
+  end
+end
+
+puts "\n\n Création de 1 Utilisateurs"
+
+user2 = User.new(first_name:"monsieur",last_name:"recruteur",email:"recruteur@gmail.com",password: "Pbxk1064",url: nil, admin: true, image: nil, company:"",number:"" ,details: "",cover_image: nil)
+
+puts "\n\n Création d'une image pour profil Utilisateurs"
+
+if user2.save
+  user2.image.attach(io: open("https://res.cloudinary.com/dbtnehfrf/image/upload/v1619518363/placeholder-profile-b8495b869b9fef8b08071042cb38316f23dde78218cff73ceb5c18875bb82911_ngdhlu.jpg"), filename:'user2')
+  print('.')
+else
+  user2 = User.where(email: "recruteur@gmail.com").first
+  if user2
+    print('-')
+  else
+    print('X')
+  end
+end
+
+puts "\n\n Création d'une image cover pour profil recruteur"
+
+if user2.save
+  user2.cover_image.attach(io: open("https://res.cloudinary.com/dbtnehfrf/image/upload/v1607285576/6jzfketmz0hlm1txj9wvahrm8szp.jpg"), filename:'user2')
+  print('.')
+else
+  user2 = User.where(email: "recruteur@gmail.com").first
+  if user2
+    print('-')
+  else
+    print('X')
+  end
+end
