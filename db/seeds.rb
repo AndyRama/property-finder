669207fd2521dccebc043a3445b0904d2c2b7074
AndyRama <andyramaroson@gmail.com>
2021-05-04 16:31:25 +0200

    skip for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,13 @@ print " Création echouée: 'X'"
 
 puts "\n\n Création de 1 Utilisateurs"
 
-user2 = User.new(first_name:"andy",last_name:"alain",email:"andy@gmail.com",password: "Pbxk1984",url: nil, admin: true, image: "https://res.cloudinary.com/dbtnehfrf/image/upload/v1619189911/vkf87wxq2a9imtiaixy7jkkf3kpw.png", company:"A2RSII",number:"0679554022" ,details: "test",cover_image: nil)
-
-puts "\n\n Création d'une image pour profil Utilisateurs"
-
-if user2.save!
-  user2.image.attach(io: open("https://res.cloudinary.com/dbtnehfrf/image/upload/v1619189911/vkf87wxq2a9imtiaixy7jkkf3kpw.png"), filename:'user2')
+andy = User.create(first_name:"anda",last_name:"alain",email:"anda@gmail.com",password: "Pbxk1984")
+if andy.save
   print('.')
 else
-  user2 = User.where(email: "andy@gmail.com").first
-  if user2
-    print('-')
-  else
-    print('X')
-  end
+  andy = User.where(email: "anda@gmail.com").first
+  print('-')
 end
 
-puts "\n\n Création d'une image cover pour profil Utilisateurs"
 
-if user1.save!
-  user1.cover_image.attach(io: open("https://res.cloudinary.com/dbtnehfrf/image/upload/v1619106936/og1hrs3zmzr65qre38sjnu205p8n.jpg"), filename:'user1')
-  print('.')
-else
-  user1 = User.where(email: "andy@gmail.com").first
-  if user1
-    print('-')
-  else
-    print('X')
-  end
-end
 
